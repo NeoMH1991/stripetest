617b6a6ddb1cfbbdaa394ecddbf2da9e720963d6
NeoMH1991 <neomenghwee1991@gmail.com>
2023-07-06 05:46:04 +0800

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+export default {
+  mode: "universal",
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
+  },
+};
